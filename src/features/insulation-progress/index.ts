0b4d9ccd3c7947755a95a574151fed27d5cb9c7a
Dmitry Egorov <bambiq@yandex.ru>
2026-08-04 21:16:28 +0300

    Добавь applyBulk для массовой отметки готовности группы

--- src/features/insulation-progress/index.ts
+++ src/features/insulation-progress/index.ts
@@ -1,3 +1,4 @@
 export { useInsulationProgress } from './model/useInsulationProgress'
 export { applyToggle } from './lib/applyToggle'
 export { isGroupDone } from './lib/isGroupDone'
+export { applyBulk } from './lib/applyBulk'
