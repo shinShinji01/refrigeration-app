35f648843e0d7045bf18c02e2a46642a1dce738b
Dmitry Egorov <bambiq@yandex.ru>
2026-08-11 17:59:25 +0300

    Add useLocalStorageState hook

--- src/shared/lib/hooks/index.ts
+++ src/shared/lib/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useMediaQuery } from './useMediaQuery'
 export { useOnClickOutside } from './useOnClickOutside'
 export { useDebounce } from './useDebounce'
+export { useLocalStorageState } from './useLocalStorageState'
